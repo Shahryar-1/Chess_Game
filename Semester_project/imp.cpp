ee3d2c0525f4d2ac98c07b1e636f8864cc7ed264
Shahryar-1 <shahryach2005@gmail.com>
2026-05-08 17:07:17 +0500

    Add Pawn

--- Semester_project/imp.cpp
+++ Semester_project/imp.cpp
@@ -1,3 +1,122 @@
 #include"sem.h"
-Piece::Piece(char color,int x,int y):color(color),x(x),y(y){}
 
+// Piece Class Implementation
+//Constructor
+Piece::Piece(char color, int x, int y) : color(color), x(x), y(y) {}
+
+// Getter for color
+char Piece::getcolor() const 
+{
+	return color;
+}
+
+//Getter for x-position
+int Piece::getx() const 
+{
+	return x;
+}
+
+//Getter for y-position
+int Piece::gety() const 
+{
+	return y;
+}
+
+// Update piece position
+void Piece::setposition(int newX, int newY)
+{
+	x = newX;
+	y = newY;
+}
+
+//virtual destructor
+Piece::~Piece(){}
+
+//=========================================================
+// PAWN CLASS IMPLEMENTATION
+//=========================================================
+
+// Constructor
+Pawn::Pawn(char color, int x, int y)
+    : Piece(color, x, y)
+{
+}
+
+
+// Returns symbol of pawn
+char Pawn::getsymbol() const
+{
+    // Uppercase for White
+    // Lowercase for Black
+    if (color == 'W')
+        return 'P';
+
+    return 'p';
+}
+
+
+// Checks if pawn move is valid
+bool Pawn::isvalidmove(int toX, int toY,
+    Piece* board[8][8])
+{
+    // White pawn moves upward
+    if (color == 'W')
+    {
+        // One step forward
+        if (toX == x - 1 && toY == y && board[toX][toY] == nullptr)
+        {
+            return true;
+        }
+
+        // First move can move two steps
+        if (x == 6 && toX == x - 2 && toY == y &&
+            board[x - 1][y] == nullptr &&
+            board[toX][toY] == nullptr)
+        {
+            return true;
+        }
+
+        // Diagonal capture
+        if (toX == x - 1 &&
+            (toY == y - 1 || toY == y + 1) &&
+            board[toX][toY] != nullptr &&
+            board[toX][toY]->getcolor() != color)
+        {
+            return true;
+        }
+    }
+
+    // Black pawn moves downward
+    else
+    {
+        // One step forward
+        if (toX == x + 1 &&
+            toY == y &&
+            board[toX][toY] == nullptr)
+        {
+            return true;
+        }
+
+        // First move can move two steps
+        if (x == 1 &&
+            toX == x + 2 &&
+            toY == y &&
+            board[x + 1][y] == nullptr &&
+            board[toX][toY] == nullptr)
+        {
+            return true;
+        }
+
+        // Diagonal capture
+        if (toX == x + 1 &&
+            (toY == y - 1 || toY == y + 1) &&
+            board[toX][toY] != nullptr &&
+            board[toX][toY]->getcolor() != color)
+        {
+            return true;
+        }
+    }
+
+    // Invalid move
+    return false;
+}
--- Semester_project/sem.h
+++ Semester_project/sem.h
@@ -1,122 +1,192 @@
 #pragma once
-#include<iostream>
+#include <iostream>
 using namespace std;
-//Base class of Piece from this every other Piece is drieved
-class Piece {
-	char color; //Colour of the piece white or black
-	int x, y;	//position of the piece at the board
+
+// BASE CLASS : Piece
+// Every chess piece will inherit from this class
+class Piece 
+{
+protected:
+    char color;   // 'W' for White , 'B' for Black
+    int x, y;     // Current position on board
+
 public:
-	Piece(char color, int x, int y);	//initializes the colour of the piece and the default position
-	char getcolor();					//getter function to get colour
-	int getx();							//return current row position
-	int gety();							//return current column position
-	void setposition();					//updates the position every time a move is made
 
-	//is a pure virtual function and will be implemented in all dereived peice classes like queen rook and kig etc
-	virtual bool isvalidmove(int toX, int toY, Piece* board[8][8]) = 0;// return true or false and tells that if the move made made was valid or not
+    // Constructor
+    Piece(char color, int x, int y);
 
-	virtual char getsymbol() = 0;
-	virtual ~Piece();	//virtual destructor
+    // Getter functions
+    char getcolor() const;
+    int getx() const;
+    int gety() const;
+
+    // Updates piece position after every move
+    void setposition(int newX, int newY);
+
+    // Pure virtual function
+    // Every derived piece must define its own movement rules
+    virtual bool isvalidmove(int toX, int toY,
+        Piece* board[8][8]) = 0;
+
+    // Returns symbol of piece
+    // Example: P,R,N,B,Q,K
+    virtual char getsymbol() const = 0;
+
+    // Virtual destructor
+    virtual ~Piece();
 };
 
+// PAWN CLASS
+class Pawn : public Piece 
+{
+public:
+    // Constructor
+    Pawn(char color, int x, int y);
 
-//simple pawn piece class inherited by Piece class implementing allt he virtual functions
-class Pawn :public Piece {
-	Pawn(char color, int x, int y);	//pawn class constructor which take values and arguments adn runs the Piece class constructor
-	bool isvalidmove(int toX, int toY, Piece* board[8][8]);	//checks if the pawn moved was valid or not
-	//like can take 1 or 2 steps at first /after the first step only step forward
-	//or if want to capture can move diagonal
-	char setsymbol();		//returns teh colour of the moved pawn
+    // Checks if pawn move is valid
+    bool isvalidmove(int toX, int toY,
+        Piece* board[8][8]) override;
+
+    // Returns symbol of pawn
+    char getsymbol() const override;
 };
 
+// ROOK CLASS
+// Moves horizontally and vertically
+class Rook : public Piece 
+{
+public:
 
-//Rook class inherited from Base Piece class
-class Rook :public Piece {
-	Rook(char color, int x, int y); //takes the colour and the default position for the rook
-	bool isvalidmove(int toX, int toY, Piece* board[8][8]);	//checks if the move is valid or not 
-	//can go updown left or right
-	char setsymbol();
+    Rook(char color, int x, int y);
+
+    bool isvalidmove(int toX, int toY,
+        Piece* board[8][8]) override;
+
+    char getsymbol() const override;
 };
 
+// BISHOP CLASS
+// Moves diagonally
+class Bishop : public Piece 
+{
 
-//Knight class inherited from Base Piece class
-class Bishop :public Piece {
-	Bishop(char color, int x, int y);	//takes the colour and the default position
-	bool isvalidmove(int toX, int toY, Piece* board[8][8]);	//checks if the move is valid or not
-	//can move in  shape 2 boxes in one direction and then one in perpendicular direction
-	char setsymbol();
+public:
+
+    Bishop(char color, int x, int y);
+
+    bool isvalidmove(int toX, int toY,
+        Piece* board[8][8]) override;
+
+    char getsymbol() const override;
 };
 
-class Knight :public Piece {
-	Knight(char color, int x, int y);	//takes the colour and the default position
-	bool isvalidmove(int toX, int toY, Piece* board[8][8]);//can only move in diagonal
-	char setsymbol();
-};
-class Queen :public Piece {
-	Queen(char color, int x, int y);	//takes the colour and the default position
-	bool isvalidmove(int toX, int toY, Piece* board[8][8]);
-	//can move in any direction diagonal straight but cannot copy the movement of knight
-	char setsymbol();
-};
+// KNIGHT CLASS
+// Moves in L-shape
+class Knight : public Piece 
+{
 
-class King :public Piece {
-	King(char color, int x, int y);	//takes the colour and the default position
-	bool isvalidmove(int toX, int toY, Piece* board[8][8]);	//can only take one step no more than one step 
-	//can take step diagonal and straight
-	char setsymbol();
-};
-//NOTE no piece of different class but same color can the take the piece of that colour
+public:
 
+    Knight(char color, int x, int y);
 
-class ChessBoard {
-	Piece* grid[8][8];// 8x8 array showing that chess columns and rows of 8 each
-	//ie index[0][0] is top left corner the default position for a rook
-	ChessBoard();    // allocates the rowns and colums for allt he 32 pieces
-	~ChessBoard();   
+    bool isvalidmove(int toX, int toY,
+        Piece* board[8][8]) override;
 
-	//prints 8x8 chess board onto the console
-	void display();  
-
-
-
-	// returns pointer to piece at position (x, y)
-	// returns nullptr if square is empty
-	// used by isValidMove to check if path is blocked
-	Piece* getPiece(int x, int y);  
-
-
-	//cals function is valid return true if move is valid or not
-	//updates the positions of the pieces and marks null ptr for each empty box||index 
-	bool movePiece(int fromX, int fromY, int toX, int toY); 
-
-
-	//loops through the chess board and finds the king of the given colour
-	// used by Game to check if the game is over
-	bool isKingAlive(char color);
+    char getsymbol() const override;
 };
 
+// QUEEN CLASS
+// Can move in all directions
+class Queen : public Piece 
+{
 
-//actual game running classs adn controls the whole game
-//this class game will act as teh cpu for the chess game
-class Game {
+public:
+
+    Queen(char color, int x, int y);
+
+    bool isvalidmove(int toX, int toY,
+        Piece* board[8][8]) override;
+
+    char getsymbol() const override;
+};
+
+// KING CLASS
+// Can move one step in any direction
+class King : public Piece 
+{
+
+public:
+
+    King(char color, int x, int y);
+
+    bool isvalidmove(int toX, int toY,
+        Piece* board[8][8]) override;
+
+    char getsymbol() const override;
+};
+
+// CHESS BOARD CLASS
+// Controls the whole board and pieces
+class ChessBoard 
+{
+
 private:
-	ChessBoard board;  
 
-	//tells which colour move is going 
-	//starts from white and switches afetr every moved
-	char currentTurn;  
+    // 8x8 board storing piece pointers
+    Piece* grid[8][8];
 
 public:
-	Game();            // initializes currentTurn to WHITE
 
+    // Constructor
+    ChessBoard();
 
-	// main game loop  keeps running until game over function returns true
-	// each iteration: displays board, asks player for input, calls movePiece
-	void start();      
+    // Destructor
+    ~ChessBoard();
 
-	//switches the turns after every move and will end when game over function return true
-	void switchTurn(); 
+    // Displays board on console
+    void display();
 
-	//checks if the game has ended and checks for the conditions of the game over like king captured and 
-	bool isGameOver(); 
+    // Returns piece at given position
+    // Returns nullptr if square is empty
+    Piece* getPiece(int x, int y);
+
+    // Checks if coordinates are inside board
+    bool isInsideBoard(int x, int y);
+
+    // Moves piece from one position to another
+    bool movePiece(int fromX, int fromY,
+        int toX, int toY,
+        char currentTurn);
+
+    // Checks if king of given color is alive
+    bool isKingAlive(char color);
+};
+
+// GAME CLASS
+// Controls complete gameplay
+class Game 
+{
+
+private:
+
+    ChessBoard board;
+
+    // Stores whose turn it is
+    // 'W' = White
+    // 'B' = Black
+    char currentTurn;
+
+public:
+
+    // Constructor
+    Game();
+
+    // Starts the game loop
+    void start();
+
+    // Switches turns after every move
+    void switchTurn();
+
+    // Checks if game is over
+    bool isGameOver();
 };
