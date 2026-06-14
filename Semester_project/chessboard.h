c747e6fac5af6f4c6bd5badd76b0e5ffc5fb5b9c
Ali Mardan <alimian1906@gmail.com>
2026-06-14 15:44:40 +0500

    change realted to sfml

--- Semester_project/chessboard.h
+++ Semester_project/chessboard.h
@@ -1,6 +1,9 @@
 #pragma once
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
 #include <iostream>
-#include <windows.h>
+#include <string>
+#include <SFML/Graphics.hpp>
 #include "Piece.h"
 using namespace std;
 
@@ -8,22 +11,31 @@ class ChessBoard
 {
 private:
     Piece* grid[8][8];
-    int enPassantX;          // row of pawn that just moved 2 steps
-    int enPassantY;          // column of pawn that just moved 2 steps
+    int  enPassantX;
+    int  enPassantY;
     bool enPassantAvailable;
+
+    sf::Texture textures[12];   // only textures stored — sprites built on the fly
+
+    void loadTextures();
+    int  getSpriteIndex(char color, char symbol) const;
 
 public:
     ChessBoard();
     ~ChessBoard();
 
     void   display();
+    void   drawSFML(sf::RenderWindow& window,
+        int selectedRow, int selectedCol,
+        bool pieceSelected);
+
     Piece* getPiece(int x, int y);
     bool   isInsideBoard(int x, int y);
     bool   movePiece(int fromX, int fromY,
         int toX, int toY,
         char currentTurn);
     bool   isKingAlive(char color);
-    bool isUnderAttack(int x, int y, char enemyColor);
-    bool isInCheck(char color);
-    bool isCheckmate(char color);
+    bool   isUnderAttack(int x, int y, char enemyColor);
+    bool   isInCheck(char color);
+    bool   isCheckmate(char color);
 };
